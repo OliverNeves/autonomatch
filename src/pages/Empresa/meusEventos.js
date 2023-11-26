74c7daeb4a79771197cbdddb297e4e13f49c8955
Oliver Neves <oliverneves1@gmail.com>
2023-11-26 12:54:21 -0300

    candidatosAtt

--- src/pages/Empresa/meusEventos.js
+++ src/pages/Empresa/meusEventos.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { FlatList, StyleSheet, TouchableOpacity, Modal, View } from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity, Modal, View, ScrollView} from 'react-native';
 import { Card, Text } from 'react-native-paper';
-import { Background, Container } from '../Login/styles';
+import { Background } from '../Login/styles';
 import { getDatabase, ref, get } from 'firebase/database';
 import { auth } from '../../contexts/firebaseConfig';
-import Icon from 'react-native-vector-icons/FontAwesome'
+import Feather from 'react-native-vector-icons/Feather'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export default function MeusEventos() {
     const [eventos, setEventos] = useState([]);
@@ -12,6 +13,18 @@ export default function MeusEventos() {
     const [selectedEvent, setSelectedEvent] = useState(null);
     const [candidaturas, setCandidaturas] = useState([]);
     const [usersData, setUsersData] = useState({});
+    const [userModalVisible, setUserModalVisible] = useState(false);
+    const [selectedUser, setSelectedUser] = useState(null);
+
+    const openUserModal = (userId) => {
+        setSelectedUser(usersData[userId]);
+        setUserModalVisible(true);
+    };
+
+    const closeUserModal = () => {
+        setSelectedUser(null);
+        setUserModalVisible(false);
+    };
 
 
     const openModal = async (event) => {
@@ -42,7 +55,7 @@ export default function MeusEventos() {
         } catch (error) {
             console.error('Erro ao buscar usuários:', error);
         }
-        console.log(usersData);
+
 
     };
 
@@ -114,7 +127,7 @@ export default function MeusEventos() {
 
 
     return (
-        <Background>
+        <><Background>
             <Text style={styles.title}>Meus Eventos</Text>
 
             {eventos.length > 0 ? (
@@ -131,51 +144,111 @@ export default function MeusEventos() {
                                 </Card.Content>
                             </Card>
                         </TouchableOpacity>
-                    )}
-                />
+                    )} />
             ) : (
                 <Text style={styles.noEventsText}>Nenhum evento encontrado.</Text>
             )}
             <Modal
-    visible={modalVisible}
-    onRequestClose={closeModal}
-    transparent={false}
-    style={styles.fullScreenModal}
->
-    <Background>
-       {selectedEvent && (
-            <View style={styles.modalContent}>
-                <Text style={styles.title}>Candidatos</Text>
-                {candidaturas.length > 0 ? (
-                    <FlatList
-                        data={candidaturas}
-                        keyExtractor={(item) => item.id}
-                        renderItem={({ item }) => {
-                            const candidato = usersData[item.userId];
-                            if (candidato) {
-                                return (
-                                    <TouchableOpacity style={styles.cardCandidato}>
-                                        <Text style={styles.nomeCandidato}>{candidato.username}</Text>
-                                        {/* Adicione outras informações conforme necessário */}
-                                    </TouchableOpacity>
-                                );
-                            } else {
-                                return null; // ou renderize algum placeholder
-                            }
-                        }}
-                    />
-                ) : (
-                    <Text>Nenhuma candidatura encontrada para este evento.</Text>
-                )}
-            </View>
-        )}
-        <TouchableOpacity style={styles.button}onPress={closeModal}>
-            <Text style={styles.inputText}>Fechar</Text>
-        </TouchableOpacity>
-    </Background>
-</Modal>
-
+                animationType="slide"
+                visible={modalVisible}
+                onRequestClose={closeModal}
+                transparent={false}
+                style={styles.fullScreenModal}
+            >
+                <Background>
+                    {selectedEvent && (
+                        <View style={styles.modalContent}>
+                            <Text style={styles.title}>Candidatos</Text>
+                            {candidaturas.length > 0 ? (
+                                <FlatList
+                                    data={candidaturas}
+                                    keyExtractor={(item) => item.id}
+                                    renderItem={({ item }) => {
+                                        const candidato = usersData[item.userId];
+                                        if (candidato) {
+                                            return (
+                                                <TouchableOpacity style={styles.cardCandidato} onPress={() => openUserModal(item.userId)}>
+                                                    <Text style={styles.nomeCandidato}>{candidato.username}</Text>
+                                                    {/* Adicione outras informações conforme necessário */}
+                                                </TouchableOpacity>
+                                            );
+                                        } else {
+                                            return null; // ou renderize algum placeholder
+                                        }
+                                    }} />
+                            ) : (
+                                <Text>Nenhuma candidatura encontrada para este evento.</Text>
+                            )}
+                        </View>
+                    )}
+                    <TouchableOpacity style={styles.button} onPress={closeModal}>
+                        <Text style={styles.inputText}>Fechar</Text>
+                    </TouchableOpacity>
+                </Background>
+            </Modal>
         </Background>
+            <Modal
+                animationType="slide"
+                visible={userModalVisible}
+                onRequestClose={closeUserModal}
+                transparent={false}
+                style={styles.fullScreenModal}
+            >
+                {selectedUser && (
+                    <Background>
+                        <TouchableOpacity
+                            style={styles.closeButton}
+                            onPress={() => closeUserModal()}
+                        >
+                            <Feather name="x" size={30} color="#fff" />
+                        </TouchableOpacity>
+                        <View style={styles.containerDados}>
+                            <View style={styles.user}>
+                                <FontAwesome name="user" size={115} color="#469CAC" />
+                            </View>
+                        </View>
+                        <View style={styles.dadosContainer}>
+                            <Text style={styles.dados}>Nome: {selectedUser.username}</Text>
+                            <Text style={styles.dados}>Telefone: {selectedUser.telefone}</Text>
+                            <Text style={styles.dados}>Email: {selectedUser.email}</Text>
+                            <Text style={styles.dados}>Data de Nascimento: {selectedUser.dtNasc}</Text>
+                            <Text style={styles.dados}>Especialidade: {
+                                selectedUser.especialidade === 'cozinheiro' ? 'Cozinheiro(a)' :
+                                    selectedUser.especialidade === 'auxiliar' ? 'Auxiliar de Cozinha' :
+                                        selectedUser.especialidade === 'garcom' ? 'Garçom / Garçonete' :
+                                            selectedUser.especialidade === 'sgerais' ? 'Serviços Gerais' :
+                                                selectedUser.especialidade
+                            }</Text>
+                            <Text style={styles.dados}>Experiência: </Text>
+                            <ScrollView style={{ borderColor: '#5B8BDF', borderWidth: 2, width: '100%', height: 150, padding: 10 }}>
+                                <Text style={styles.dados}>{selectedUser.experiencia}</Text>
+                            </ScrollView>
+                            <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+                            <TouchableOpacity
+                                style={styles.contratar}
+                                onPress={() => {
+                                    // Adicione aqui a lógica para se candidatar ao evento
+                                    setSelectedUser(null);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>Contratar</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                style={styles.contratar}
+                                onPress={() => {
+                                    // Adicione aqui a lógica para se candidatar ao evento
+                                    setSelectedUser(null);
+                                }}
+                            >
+                                <Text style={styles.textStyle}>Recusar</Text>
+                            </TouchableOpacity>
+                            
+                            </View>
+                        </View>
+
+                    </Background>
+                )}
+            </Modal></>
     );
 };
 
@@ -212,7 +285,7 @@ const styles = StyleSheet.create({
         margin: 10,
         borderRadius: 10,
     },
-    button:{
+    button: {
         width: 100,
         height: 50,
         borderRadius: 20,
@@ -222,10 +295,55 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginTop: 10
-        
+
     },
-    inputText:{
+    inputText: {
         color: 'white',
         fontSize: 20,
-    }
+    },
+    dados: {
+        color: '#fff',
+        fontSize: 20,
+
+    },
+    dadosContainer: {
+        padding: 10,
+
+    },
+
+    containerDados: {
+        alignItems: 'center',
+        padding: 25
+    },
+    contratar: {
+        backgroundColor: "#121212",
+        alignSelf: 'center',
+        width: 110,
+        borderRadius: 25,
+        marginTop: 20,
+        padding: 10
+
+    },
+    textStyle: {
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        fontSize: 20
+    },
+
+    user: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        marginTop: 30,
+        borderRadius: 100,
+        height: 130,
+        width: 130,
+        marginBottom: 20,
+    },
+    closeButton: {
+        position: 'absolute',
+        top: 10,
+        right: 10,
+        zIndex: 1,
+    },
 });
